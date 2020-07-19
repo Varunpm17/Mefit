129a1b1fb7975ca1b355a22609ed3eb5911397e2
GauravMohan1 <51386698+GauravMohan1@users.noreply.github.com>
2020-07-18 19:04:59 -0700

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,14 +6,42 @@ from pydrive.drive import GoogleDrive
 import os
 
 gauth = GoogleAuth()
-gauth.LocalWebserverAuth()
+
+# Try to load saved client credentials
+gauth.LoadCredentialsFile("mycreds.txt")
+
+if gauth.credentials is None:
+    # Authenticate if they're not there
+
+    # This is what solved the issues:
+    gauth.GetFlow()
+    gauth.flow.params.update({'access_type': 'offline'})
+    gauth.flow.params.update({'approval_prompt': 'force'})
+
+    gauth.LocalWebserverAuth()
+
+elif gauth.access_token_expired:
+
+    # Refresh them if expired
+
+    gauth.Refresh()
+else:
+
+    # Initialize the saved creds
+
+    gauth.Authorize()
+
+# Save the current credentials to a file
+gauth.SaveCredentialsFile("mycreds.txt")  
+
 drive = GoogleDrive(gauth)
+
 
 MIMETYPES = {
         # Drive Document files as MS dox
         'application/vnd.google-apps.document': 'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
         # Drive Sheets files as MS Excel files.
-        'application/vnd.google-apps.spreadsheet': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+        'application/vnd.google-apps.spreadsheet': 'text/csv',
         # Drive presentation as MS pptx
         'application/vnd.google-apps.presentation': 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
         # see https://developers.google.com/drive/v3/web/mime-types
@@ -27,7 +55,10 @@ EXTENSTIONS = {
 f = open("failed.txt","w+")
 folder_id = '1jR4xAsyjRueHfBG4yGpA5P7BSPMq-kaK'
 root = 'drive_download'
-os.mkdir(root)
+try:
+    os.mkdir(root)
+except FileExistsError:
+    pass
 
 def escape_fname(name):
     return name.replace('/','_')
@@ -41,6 +72,7 @@ def search_folder(folder_id, root):
             foldername = escape_fname(file['title'])
             create_folder(root,foldername)
             search_folder(file['id'], '{}{}/'.format(root,foldername))
+    
         else:
             download_mimetype = None
             filename = escape_fname(file['title'])
@@ -59,8 +91,12 @@ def search_folder(folder_id, root):
                 f.write(filename+'\n')
 
 def create_folder(path,name):
-    os.mkdir('{}{}'.format(path,escape_fname(name)))
+    try:
+        os.mkdir('{}{}'.format(path,escape_fname(name)))
+    except FileExistsError:
+        return
 
 search_folder(folder_id,root+'/')
 f.close() 
+
 
